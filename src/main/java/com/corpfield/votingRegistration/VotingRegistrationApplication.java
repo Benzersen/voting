5c5c04f6aa98c220b7d7ccff515b83cb99d4c92a
benzersen <benzersen@gmail.com>
2022-02-09 15:08:57 +0530

    init commit

---
+++ src/main/java/com/corpfield/votingRegistration/VotingRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.corpfield.votingRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corpfield/votingRegistration/VotingRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corpfield.votingRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
